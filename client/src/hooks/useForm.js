01257bebfedb0baa9849ae19608ef54db9d2c20c
Taylor Roebuck <taylornroebuck@gmail.com>
2020-05-16 09:48:43 -0700

    built useForm custom hook

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,16 @@
 // write your custom hook here to control your checkout form
+import { useState } from 'react';
+
+export const useForm = (initialValue) => {
+    const [value, setValue] = useState(initialValue);
+
+    const handleChanges = e => {setValue({...value, [e.target.name]: e.target.value});
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        setShowSuccessMessage(true);
+    }
+
+    return [value, handleChanges];
+}
